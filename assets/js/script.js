d746fc56d78f9a79b26f2dfe040b5863571ba567
Reknan-dev <benedettocaiazzo8409@hotmail.com>
2023-04-24 14:02:06 +0200

    alt and works

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,19 @@
 function SendMail() {
   var params = {
     from_name : document.getElementById("name").value,
-    email : document.getElementById("email").value,
-    message : document.getElementById("message").value
+    email_id : document.getElementById("email").value,
+    message : document.getElementById("message").value,
   }
-  emailjs.send("service_csnm5mw", "template_wltyylm", params).then(function (res) {
-    alert("Success! " + res.status);
-
-  })
+  emailjs.send("service_umfncfb", "template_wltyylm", params)
+    .then(function (res) {
+    alert("Messaggio inviato correttamente!" + res.status);
+  }).catch(e => alert(e.message))
 
 }
+
+const button = document.getElementById("sendMail")
+button.addEventListener('click', function(event) {
+  event.preventDefault()
+  SendMail()
+})
+
